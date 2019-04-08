02c4da19036e47dde535927418cb3131acf433df
Tony Flury <anthony.flury@btinternet.com>
2019-04-08 11:38:22 +0100

    Set release number to 0.2.1rc1 and version to 0.2.1

--- templatelite/version.py
+++ templatelite/version.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
-__version__ = '0.2.0a8'
-__release__ = '0.2.0'
+__version__ = '0.2.1'
+__release__ = '0.2.1rc1'
 __copyright__ = u'©2018-2019 : Tony Flury'
 __author__ = u'Tony Flury'
 __email__ = u'anthony.flury@btinternet.com'
